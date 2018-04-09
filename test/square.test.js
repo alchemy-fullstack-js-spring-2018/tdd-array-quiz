234a079c823f630fe971c6b32e57c8dbcd4dbd0b
Steele Walston <steele.walston@gmail.com>
2018-04-09 15:26:41 -0700

    returns squared array with values greater than 20

--- test/square.test.js
+++ test/square.test.js
@@ -1,9 +1,18 @@
 const assert = require('assert');
-const arr = require('../lib/square');
+const square = require('../lib/square');
 
 describe('array test', () => {
+    const arr = [1, 4, 5, 7];
+    const squared = x => x * x; 
 
-    it('squares array and returns array with values greater then 20', () => {
+    it('squares number in array', () => {
+        const allSquared = square(arr, squared);
+        assert.deepEqual(allSquared, [1, 16, 25, 49]);
+    });
+
+    it.only('squares array and returns array with values greater then 20', () => {
+        const newArray = square(arr, squared);
+        assert.deepEqual(newArray, [25, 49]);
 
     });
 
