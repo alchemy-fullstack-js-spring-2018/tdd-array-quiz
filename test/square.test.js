8b19dccb93c5af753cf86bbb7d8116b5dc268159
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-09 15:17:58 -0700

    updated name to squared

--- test/square.test.js
+++ test/square.test.js
@@ -5,8 +5,7 @@ describe('every', () => {
 
     it('returns an array of the squares larger than 25', () => {
         const arr = [1, 4, 5, 7];
-        const name = square(arr);
-        assert.deepEqual(name, [25, 49]);
+        const squared = square(arr);
+        assert.deepEqual(squared, [25, 49]);
     });
-
 });
