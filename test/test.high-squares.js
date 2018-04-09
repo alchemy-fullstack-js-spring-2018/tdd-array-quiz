db8f70c46d154c118bd322445d1f1819d333a258
CharlyWelch <blwbiology@gmail.com>
2018-04-09 15:17:59 -0700

    fails test for high squares

---
+++ test/test.high-squares.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const highSquares = require('../lib/high-squares');
+
+describe('Array squares over 20', () => {
+
+    it('takes array, returns array of squares > 20', () => {
+        const array = [1, 4, 5, 7];
+        const result = highSquares(array);
+        assert.equal(result, [25, 49]);
+    });
+});
