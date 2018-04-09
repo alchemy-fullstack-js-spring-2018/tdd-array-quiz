df2708361e9e057c186df96f242e002c525bb793
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-09 15:28:40 -0700

    square function

--- test/array-test.js
+++ test/array-test.js
@@ -6,7 +6,7 @@ describe('testing the array', () =>{
     it('test to make sure array is being squared', () =>{
 
         const testArray = [1, 4, 5, 7];
-        const newArray = array.map(testArray, x => (x * x));
+        const newArray = array.square(testArray, x => (x * x));
         assert.deepEqual(newArray, [1, 16, 25, 49]);
 
     })
