da58fd2730d76114ddf99075bda52583546a1287
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-09 15:26:55 -0700

    removed newLocal

--- test/array-test.js
+++ test/array-test.js
@@ -3,11 +3,11 @@ const array = require('../lib/array');
 
 describe('testing the array', () =>{
 
-    it('test to make sure arrays are iterating', () =>{
+    it('test to make sure array is being squared', () =>{
 
         const testArray = [1, 4, 5, 7];
         const newArray = array.map(testArray, x => (x * x));
-        assert.deepEqual(newArray, [25, 49]);
+        assert.deepEqual(newArray, [1, 16, 25, 49]);
 
     })
 
