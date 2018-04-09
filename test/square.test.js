f2faedf828f1ddf015f036f30adb539635a1f998
Steele Walston <steele.walston@gmail.com>
2018-04-09 15:28:15 -0700

    clean up

--- test/square.test.js
+++ test/square.test.js
@@ -5,12 +5,7 @@ describe('array test', () => {
     const arr = [1, 4, 5, 7];
     const squared = x => x * x; 
 
-    it('squares number in array', () => {
-        const allSquared = square(arr, squared);
-        assert.deepEqual(allSquared, [1, 16, 25, 49]);
-    });
-
-    it.only('squares array and returns array with values greater then 20', () => {
+    it('squares array and returns array with values greater then 20', () => {
         const newArray = square(arr, squared);
         assert.deepEqual(newArray, [25, 49]);
 
