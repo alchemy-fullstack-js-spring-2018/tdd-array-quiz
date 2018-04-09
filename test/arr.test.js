0b58098e81b03e79ad096a4482550b34bddc5480
Charlie <heinercj@gmail.com>
2018-04-09 15:08:06 -0700

    initial set up

---
+++ test/arr.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const arrFilter = require('../lib/arrFilter');
+
+describe('it test array function', () => {
+
+    it('tests that function squares and filters arr', () => {
+        
+    });
+});
