b727d6118d14065ce82930a478167540a1e76c08
pereztjacob <pereztjacob@gmail.com>
2018-04-09 15:18:47 -0700

    second test added and pretest lint

--- test/arr-methods.test.js
+++ test/arr-methods.test.js
@@ -10,4 +10,12 @@ describe('array methods', () => {
 
         assert.deepEqual(result, [25, 49]);
     });
+
+    it('second square and check', () => {
+        let arr = [1, 1, 1, 1, 1, 22];
+
+        const result = squares(arr);
+       
+        assert.deepEqual(result, [484]);
+    });
 });
