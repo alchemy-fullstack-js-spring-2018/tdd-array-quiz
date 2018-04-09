95217dc2a2ce38b7505349047fb831914936f764
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-09 15:22:43 -0700

    updated the test description

--- test/square.test.js
+++ test/square.test.js
@@ -3,7 +3,7 @@ const square = require('../lib/square');
 
 describe('every', () => {
 
-    it('returns an array of the squares larger than 25', () => {
+    it('returns an array of the squares larger than 20', () => {
         const arr = [1, 4, 5, 7];
         const squared = square(arr);
         assert.deepEqual(squared, [25, 49]);
