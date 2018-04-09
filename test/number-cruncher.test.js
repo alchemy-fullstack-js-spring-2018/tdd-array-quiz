f6fa4938b49081ecba4679e0f56d207e8aacd76f
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-09 15:18:56 -0700

    specify integers (because otherwise I would filter after squaring)

--- test/number-cruncher.test.js
+++ test/number-cruncher.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const numberCruncher = require('../lib/number-cruncher');
 
 describe('number cruncher', () => {
-    it('takes an array of numbers and returns a new array that contains the square of the input numbers filtered to only contain those squares greater than 20', () => {
+    it('takes an array of integers and returns a new array that contains the square of the input numbers filtered to only contain those squares greater than 20', () => {
         const returned = numberCruncher([1, 4, 5, 7]);
         assert.deepEqual([25, 49], returned);
     });
