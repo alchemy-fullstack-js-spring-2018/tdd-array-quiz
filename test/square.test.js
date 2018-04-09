c5b32ad843f772d1f072d450222322706297c876
Steele Walston <steele.walston@gmail.com>
2018-04-09 15:09:42 -0700

    npm all good to go and start files created

---
+++ test/square.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const arr = require('../lib/square');
+
+describe('array test', () => {
+
+    it('squares array and returns array with values greater then 20', () => {
+
+    });
+
+}); 
