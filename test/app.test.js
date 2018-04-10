87be030474111002efdbfffbe1b70674cd8b6b54
victorbofill <victorm.bofill@gmail.com>
2018-04-10 12:54:23 -0700

    Refactored the test

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,18 @@ const assert = require('assert');
 const app = require('../lib/app');
 
 const testArray = [1, 4, 5, 7];
+const testArray2 = [9, 4, 1, 10];
 
 describe('Testing squaresTest function', () => {
     it('Test 1', () => {
         const results = app.squaresTest(testArray);
 
         assert.deepEqual(results, [25, 49]);
+    });    
+
+    it('Test 2', () => {
+        const results = app.squaresTest(testArray2);
+
+        assert.deepEqual(results, [81, 100]);
     });    
 });
